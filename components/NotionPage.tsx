ee6b63c3d6d4dfe72db8dea875051a93254a6dfc
beta <beta.underalpha@gmail.com>
2022-01-22 17:43:14 +0900

    remove: GitHub share button

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -32,7 +32,6 @@ import { PageHead } from './PageHead'
 import { PageActions } from './PageActions'
 import { Footer } from './Footer'
 import { PageSocial } from './PageSocial'
-import { GitHubShareButton } from './GitHubShareButton'
 import { ReactUtterances } from './ReactUtterances'
 
 import styles from './styles.module.css'
@@ -280,7 +279,6 @@ export const NotionPage: React.FC<types.PageProps> = ({
         }
       />
 
-      <GitHubShareButton />
     </TwitterContextProvider>
   )
 }
